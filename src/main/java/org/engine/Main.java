e44b8a67bd2589a2e6a7a4e694d8363e91a3cd0b
WafflzPlz <michael.kogler1@gmx.at>
2025-04-13 20:20:41 +0200

    initial commit, tut 1 finished

---
+++ src/main/java/org/engine/Main.java
@@ -0,0 +1,11 @@
+package org.engine;
+
+import org.engine.jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/org/engine/jade/Window.java
@@ -0,0 +1,102 @@
+package org.engine.jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_MAXIMIZED;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window;
+
+    private Window() {
+        this.height = 1080;
+        this.width = 1920;
+        this.title = "Mario";
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Setup error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        // Configure GLFW
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED,  GLFW_TRUE);
+
+        // Create Window
+        this.glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (this.glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(this.glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(this.glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while(!glfwWindowShouldClose(this.glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 0f, 0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(this.glfwWindow);
+        }
+    }
+}
